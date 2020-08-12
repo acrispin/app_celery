f0a846e3dddb7f70707cbeda0aae5de25c29f7c9
Antonio Crispin <antonio.crispin@gmail.com>
2020-08-12 03:09:02 -0500

    task bind

--- src/tasks.py
+++ src/tasks.py
@@ -7,9 +7,10 @@ logger = logging.getLogger(__name__)
 logger.addHandler(fileHandler)
 
 
-@app.task
-def longtime_add(first, second):
-    logger.info("Inicio de tarea, esperando 5 segundos")
+# https://docs.celeryproject.org/en/latest/userguide/tasks.html#bound-tasks
+@app.task(bind=True)
+def longtime_add(self, first, second):
+    logger.info("Inicio de tarea, esperando 5 segundos, id: " + self.request.id)
     time.sleep(5)
     logger.info('Finalizacion de tarea')
     return f"{first} + {second} = {first + second}"
