692cf71455aa4439e52318bc509ec05ac1966b8f
Antonio Crispin <antonio.crispin@gmail.com>
2020-08-14 17:18:17 -0500

    sentry

--- src/log.py
+++ src/log.py
@@ -12,12 +12,10 @@ MAX_BYTES = 50*1024*1024  # 50MB
 BACKUP_COUNT = 10
 DEBUG = config('DEBUG', default=False, cast=bool)
 LOGGER_LEVEL = logging.DEBUG if DEBUG else logging.INFO
-FORMAT_LOGGER_TEXT = '[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] - %(message)s'
-
+FORMAT_LOGGER_TEXT = '%(levelname)s [%(asctime)s] [%(name)s.%(funcName)s:%(lineno)d] -> %(message)s'
 
 if not os.path.isdir(DIR_LOGS):
     os.mkdir(DIR_LOGS)
-
 
 logging.basicConfig(
     level=LOGGER_LEVEL,
@@ -33,3 +31,13 @@ fileHandler.setLevel(LOGGER_LEVEL)
 fileHandler.setFormatter(formatter)
 
 logging = logging
+
+SENTRY_DSN = config('SENTRY_DSN', default='', cast=str)
+if SENTRY_DSN:
+    # https://sentry.io/siunicon-dev/360_python_dev/getting-started/python/
+    import sentry_sdk
+    if not sentry_sdk.Hub.current.client:
+        sentry_sdk.init(SENTRY_DSN)
+        logger = logging.getLogger(__name__)
+        logger.addHandler(fileHandler)
+        logger.info("Se configura sentry")
